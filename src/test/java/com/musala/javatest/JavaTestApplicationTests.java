b2a8cbd16cf1488ebfeec4a8dd8b688f80655d4d
tinashediego <tinashediego@gmail.com>
2023-03-27 08:43:57 +0200

    project creation

---
+++ src/test/java/com/musala/javatest/JavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.javatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
